c2c758abcd044122d83980cf04a5a06f37fa91aa
poison <shahsusan291@gmail.com>
2024-12-02 08:38:59 +0545

    Reinitialized

--- Server.js
+++ Server.js
@@ -1,29 +1,25 @@
-import express from 'express'
+import express from "express";
 
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import anime from './Routes/anime.js'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import anime from "./Routes/anime.js";
 
+dotenv.config();
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-dotenv.config()
-mongoose.connect(process.env.DATABASE_URL,  { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected To Database"));
 
-const db = mongoose.connection
-db.on('error', (error) =>console.error(error))
-db.once('open', () => console.log('Connected To Database'))
+const app = express();
 
+app.use(express.json());
 
-const app = express()
-
-
-
-app.use(express.json())
-
-app.use('/anime', anime)
-
-
+app.use("/anime", anime);
 
 app.listen(4000, () => {
-    console.log(`App running on 4000`);
-    
-})
+  console.log(`App running on 4000`);
+});
